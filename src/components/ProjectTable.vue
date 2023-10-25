61d7ce0db32838c14154a673dcf3cf88821ea220
Marvellous Sam-kayode <marvelloussamkayode@gmail.com>
2023-10-25 13:23:43 +0100

    add project table

---
+++ src/components/ProjectTable.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="table-container">
+        <table class="static">
+            <thead>
+                <tr>
+                    <th>Static Column 1</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1</td>
+                </tr>
+                <!-- Add more rows as needed -->
+            </tbody>
+        </table>
+        <div class="scrollable">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Scrollable Column 2</th>
+                        <th>Scrollable Column 3</th>
+                        <th>Scrollable Column 4</th>
+                        <th>Scrollable Column 5</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 4</td>
+                        <td>Data 5</td>
+                    </tr>
+                    <!-- Add more rows as needed -->
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
